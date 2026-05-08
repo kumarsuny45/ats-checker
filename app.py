48413e4f563ab390d90127491b730d18a957c327
Leena Arora <la1m23@soton.ac.uk>
2026-05-08 11:10:29 +0100

    fixed render blank page

--- app.py
+++ app.py
@@ -75,4 +75,9 @@ with gr.Blocks() as demo:
 
 # -------------------- RUN --------------------
 
-demo.launch(server_name="0.0.0.0", server_port=8080)
+if __name__ == "__main__":
+    demo.launch(
+        server_name="0.0.0.0",
+        server_port=8080,
+        share=False
+    )
